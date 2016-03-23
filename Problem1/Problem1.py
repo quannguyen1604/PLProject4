692ae402813f0f8da7a861039873342e7d3c61ae
Quan Anh Nguyen <quannguyen1604@gmail.com>
2016-03-23 16:02:10 -0500

    completed problem 1

--- Problem1/Problem1.py
+++ Problem1/Problem1.py
@@ -39,18 +39,23 @@ s_dept = (('ID','NAME','REGION_ID'),
           (45,'Operations',5),
           (50,'Administration',1))
 
-
+#1
 print "\nselect * from s_dept: ", [[i[0],i[1],i[2]] for i in s_dept[1::]]
-
-print "\nselect last_name, first_name, title, salary from s_emp; ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::]]
-
-print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40; ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40]
-
-print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by last_name; ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:x[0])
-
-print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc; ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:int(x[3]),reverse = True)
-
-print "\nselect dept_id, avg(salary) from s_emp group by dept_id order by dept_id"
+#2
+print "\nselect last_name, first_name, title, salary from s_emp: ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::]]
+#3
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40: ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40]
+#4
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by last_name: ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:x[0])
+#5
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc: ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:int(x[3]),reverse = True)
+#6
+print "\nselect last_name, first_name, title, salary, name from s_emp e join s_dept d on(e.dept_id = d.id): ", [ [i[1], i[2], i[6], i[7], j[1]] for i in s_emp[1::] for j in s_dept[1::] if i[9] == j[0] ]
+#7
+print "\nselect dept_id, avg(salary) from s_emp group by dept_id order by dept_id: "
 for department in sorted({ d[9] for d in s_emp[1::] }): print (department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
-
+#Another approach for 7: print sorted([ (lambda deptId, avgSal: (deptId, avgSal))(department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ]))) for department in { d[9] for d in s_emp[1::]} ], key = lambda x : x[0])
+#8
+print "\nselect dept_id, avg(salary) from s_emp group by dept_id having avg(salary) < 1500: "
+for department in { d[9] for d in s_emp[1::] }: print (lambda deptId, avgSal: (deptId, avgSal) if avgSal < 1500 else '')(department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
 
