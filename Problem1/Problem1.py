c191c8d5685563eb0e72e415094bf38cae6e3668
Ryan Wechter <ryan.wechter@utexas.edu>
2016-03-23 15:38:12 -0500

    Finished Project 4

--- Problem1/Problem1.py
+++ Problem1/Problem1.py
@@ -41,3 +41,16 @@ s_dept = (('ID','NAME','REGION_ID'),
 
 
 print "\nselect * from s_dept: ", [[i[0],i[1],i[2]] for i in s_dept[1::]]
+
+print "\nselect last_name, first_name, title, salary from s_emp; ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::]]
+
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40; ", [[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40]
+
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by last_name; ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:x[0])
+
+print "\nselect last_name, first_name, title, salary from s_emp where salary > 1500 and dept_id > 40 order by salary desc; ", sorted([[i[1],i[2],i[6],i[7]] for i in s_emp[1::] if i[7] > 1500 and i[9] > 40],key = lambda x:int(x[3]),reverse = True)
+
+print "\nselect dept_id, avg(salary) from s_emp group by dept_id order by dept_id"
+for department in sorted({ d[9] for d in s_emp[1::] }): print (department, (lambda l: round(sum(l) / len(l), 2))(map(float,[ e[7] for e in s_emp[1::] if e[9] == department ])))
+
+
